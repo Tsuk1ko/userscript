ae8b58cdd4269d0e5426697e33f5e577831ee79b
神代綺凛 <i@loli.best>
2024-05-28 00:00:39 +0800

    feat: 增加 x.com

--- src/twitter-remove-content-warning/index.js
+++ src/twitter-remove-content-warning/index.js
@@ -4,11 +4,12 @@
 // @name:zh-TW   Twitter 移除内容警告
 // @icon         https://twitter.com/favicon.ico
 // @namespace    https://github.com/Tsuk1ko
-// @version      1.0.1
+// @version      1.0.2
 // @description        Remove twitter content warning
 // @description:zh-CN  移除 twitter 的敏感内容警告
 // @description:zh-TW  移除 twitter 的敏感内容警告
 // @author       神代綺凛
+// @include      https://x.com/*
 // @include      https://twitter.com/*
 // @include      https://mobile.twitter.com/*
 // @license      MIT
